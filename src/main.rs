04eaab77a0fd3412a34592b3f219a238fdf55864
Szabó István <pisti4395@gmail.com>
2024-04-26 22:30:54 +0200

    a bit of a rewrite
    a little rewrite, made it a little nicer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand, ValueEnum};
+use std::str::FromStr;
 use crate::monitor::get_by_id;
 use std::env;
 
@@ -16,7 +17,17 @@ pub fn get_current_monitor() -> Monitor {
     monitor::get().find(|m| m.focused).unwrap()
 }
 
-fn get_target(direction: &str, current: i32) -> i32 {
+fn detect_order() -> Vec<i16>{
+    let mut order : Vec<Monitor> = Vec::new();
+    for i in monitor::get() {
+        order.push(i)
+    } 
+    order.sort_by_key(|a| a.x);
+    let ordered_ids : Vec<i16 >= order.iter().map(|mon| mon.id).collect();
+    return ordered_ids;
+}
+
+fn get_target_old(direction: &str, current: i32) -> i32 {
     let mut id = 1;
     if direction == "l" {
         if current == 2{
@@ -34,61 +45,103 @@ fn get_target(direction: &str, current: i32) -> i32 {
     return id
 }
 
-fn main() {
-    let args: Vec<_> = env::args().collect();
-
-	if args[1] == "custom"{
-        let current = get_current_monitor().id;
-        if args[2] == "c" { // for current
-			println!("{} {}", get_by_id(current).active_workspace.id, current);
-
-        // move the focus to the left monitor
-        } else if args[2] == "l" || args[2] == "r" {
-            let id = get_target(&args[2], current.into());
-			Command::new("hyprctl").arg(format!("dispatch workspace {id}")).output().expect("fasz").stdout.as_slice();
-    	
-        // move the focused window to the left or right monitor and follow it
-        } else if args[2] == "m" {
-			if args[3] == "r" || args[3] == "l" {
-                let id = get_target(&args[3], current.into());
-                Command::new("hyprctl").arg(format!("dispatch movetoworkspace {id}")).output().expect("fasz").stdout.as_slice();
-            }
-        // move the focused window to the left or right monitor but dont follow it
-    	} else if args[2] == "n" {
-			if args[3] == "r" || args[3] == "l" {
-                let old_id = get_by_id(current).active_workspace.id;
-                let id = get_target(&args[3], current.into());
-                Command::new("hyprctl").arg(format!("dispatch movetoworkspace {id}")).output().expect("fasz").stdout.as_slice();
-                Command::new("hyprctl").arg(format!("dispatch workspace {old_id}")).output().expect("fasz").stdout.as_slice();
-            }
-		// move the focused window to the next of prev workspace on that monitor and follow it
-        } else if args[2] == "w" {
-			if args[3] == "r" || args[3] == "l" {
-                let mut id = get_by_id(current).active_workspace.id;
-                if ((current)*10+1..current*10+11).contains(&(id as i16)) {
-                    if args[3] == "r" {
-                        id = id+1
+fn get_target(order: Vec<i16>, current: i16, direction: &str) -> i16 {
+	for i in 0..order.len() {
+        if current == order[i] {
+            match direction {
+                "l" => {
+                    if i == 0 {
+                        let target = get_by_id(order[usize::try_from(i).unwrap()]).active_workspace.id.try_into().unwrap();
+                        println!("target {} current {}", target, current);
+                        return target
                     } else {
-                        id = id-1
+                        let target = get_by_id(order[usize::try_from(i-1).unwrap()]).active_workspace.id.try_into().unwrap();
+                        println!("target {} current {}", target, current);
+                    	return target
                     }
-                	Command::new("hyprctl").arg(format!("dispatch movetoworkspace {id}")).output().expect("fasz").stdout.as_slice();
-                }
-            }
-        } else if args[2] == "b" {
-			if args[3] == "r" || args[3] == "l" {
-                let mut id = get_by_id(current).active_workspace.id;
-                let old_id = id;
-                if ((current)*10+1..current*10+11).contains(&(id as i16)) {
-                    if args[3] == "r" {
-                        id = id+1
+                },
+                "r" => {
+                    if i == order.len()-1 {
+                        let target = get_by_id(order[usize::try_from(i).unwrap()]).active_workspace.id.try_into().unwrap();
+                        println!("target {} current {}", target, current);
+                        return target
                     } else {
-                        id = id-1
+                        let target = get_by_id(order[usize::try_from(i+1).unwrap()]).active_workspace.id.try_into().unwrap();
+                        println!("target {} current {}", target, current);
+                        return target
                     }
-                	Command::new("hyprctl").arg(format!("dispatch movetoworkspace {id}")).output().expect("fasz").stdout.as_slice();
-                	Command::new("hyprctl").arg(format!("dispatch workspace {old_id}")).output().expect("fasz").stdout.as_slice();
                 }
+                _ => return 0
+                // Some(_) => return 0,
+                // None => return 0,
             }
         }
-        return
+    }
+    return 0
+}
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    let order = detect_order();
+
+    let current = get_current_monitor().id;
+    match args[1].as_str() {
+        "c" => {
+			println!("{}", current);
+        },
+        "l" | "r" => {
+       		// move the focus to the left monitor
+       		let id = get_target(order, current.into(), &args[1]);
+       		Command::new("hyprctl").arg(format!("dispatch workspace {id}")).output().expect("fasz").stdout.as_slice();
+        },
+        "m" => {
+        	// move the focused window to the left or right monitor and follow it
+        	if args[2] == "r" || args[2] == "l" {
+        	    let id = get_target(order, current.into(), &args[2]);
+        	    Command::new("hyprctl").arg(format!("dispatch movetoworkspace {id}")).output().expect("fasz").stdout.as_slice();
+        	}
+        },
+        "n" => {
+        	// move the focused window to the left or right monitor but dont follow it
+        	if args[2] == "r" || args[2] == "l" {
+        	    let old_id = get_by_id(current).active_workspace.id;
+        	    let id = get_target(order, current.into(), &args[2]);
+        	    Command::new("hyprctl").arg(format!("dispatch movetoworkspace {id}")).output().expect("fasz").stdout.as_slice();
+        	    Command::new("hyprctl").arg(format!("dispatch workspace {old_id}")).output().expect("fasz").stdout.as_slice();
+        	}
+        },
+        "w" => {
+        	// move the focused window to the next of prev workspace on that monitor and follow it
+        	if args[2] == "r" || args[2] == "l" {
+        	    let mut id = get_by_id(current).active_workspace.id;
+        	    if ((current)*10+1..current*10+11).contains(&(id as i16)) {
+        	        if args[2] == "r" {
+        	            id = id+1
+        	        } else {
+        	            id = id-1
+        	        }
+        	        Command::new("hyprctl").arg(format!("dispatch movetoworkspace {id}")).output().expect("fasz").stdout.as_slice();
+        	    }
+        	}
+        },
+		"b" => {
+        	if args[2] == "r" || args[2] == "l" {
+        	    let mut id = get_by_id(current).active_workspace.id;
+        	    let old_id = id;
+        	    if ((current)*10+1..current*10+11).contains(&(id as i16)) {
+        	        if args[2] == "r" {
+        	            id = id+1
+        	        } else {
+        	            id = id-1
+        	        }
+        	        Command::new("hyprctl").arg(format!("dispatch movetoworkspace {id}")).output().expect("fasz").stdout.as_slice();
+        	        Command::new("hyprctl").arg(format!("dispatch workspace {old_id}")).output().expect("fasz").stdout.as_slice();
+        	    }
+        	}
+        },
+        "d" => {
+            println!("{:?}", detect_order());
+        },
+        _ => {}
     }
 }
